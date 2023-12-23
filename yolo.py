c78ea8fbd37624cf64e7405819aeea536670f388
mrasyadc <mrcrasyad26@gmail.com>
2023-12-23 11:50:01 +0700

    add yolo

---
+++ yolo.py
@@ -0,0 +1,59 @@
+from ultralytics import YOLO
+import cv2
+import math 
+# start webcam
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+
+model_dir = "../yolov8-full-best.pt"
+
+# model
+model = YOLO(model_dir)
+
+# object classes
+classNames = ["Varian 60 ml", "Varian 63 ml"]
+
+
+while True:
+    success, img = cap.read()
+    results = model(img, stream=True)
+
+    for r in results:
+        boxes = r.boxes
+
+        for box in boxes:
+            # bounding box
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
+
+            # put box in cam
+            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+
+            # confidence
+            confidence = math.ceil((box.conf[0]*100))/100
+            print("Confidence --->",confidence)
+
+            # class name
+            cls = int(box.cls[0])
+            print("Class name -->", classNames[cls])
+
+            # object details
+            org = [x1, y1]
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            fontScale = 1
+            color = (255, 0, 0)
+            thickness = 2
+
+            cv2.putText(img, classNames[cls] + " " + str(confidence*100) + "%", org, font, fontScale, color, thickness)
+
+    # Display the frame
+    cv2.imshow('Real-time Classification', img)
+
+    # Break the loop if 'q' key is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the video capture object and close all windows
+cap.release()
+cv2.destroyAllWindows()
